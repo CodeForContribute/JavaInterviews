cd2be3a5ac9e59da67a182dc2e47d61b6dcc74f8
Raushan Kumar <raushkumar@paypal.com>
2024-01-03 12:16:30 +0530

    more threading concepts

---
+++ src/main/java/concepts/and/coding/videos/concurrency/MonitorLockProducerConsumerDemo.java
@@ -0,0 +1,15 @@
+package concepts.and.coding.videos.concurrency;
+
+public class MonitorLockProducerConsumerDemo {
+    public static void main(String[] args) {
+        System.out.println("Main method start >>>>>>>>>>>>>>>");
+        SharedResource sharedResource = new SharedResource();
+        // producer thread
+        Thread producerThread = new Thread(new ProducerTask(sharedResource));
+        // consumer thread
+        Thread consumerThread = new Thread(new ConsumerTask(sharedResource));
+        producerThread.start();
+        consumerThread.start();
+        System.out.println("Main method end >>>>>>>>>>>>>");
+    }
+}
